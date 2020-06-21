369850245bf93f211a78d0ddda739b4ed519f99a
Constanza <cotycirion94@gmail.com>
2020-06-21 20:55:23 -0300

    agrego datos de configuración de Twilio a archivo config.js

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 
 export default {
   purchasesPdfUrl: "src/server/pdf/generated",
-  fileStorageUrl: "http://localhost:5000/"
+  fileStorageUrl: "http://localhost:5000/",
+  twilioConfig = {
+    accountSid :'AC4ad2c99f8af10c473575dde6d9dedfd6',
+    authToken :'d932ded7e53fd146f730fc9ea225ebd7',
+    sender :'+12038710956'
+}
 }
