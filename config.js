0364929405e1a3b16da7d787847c2c350451bf24
unknown <ivan.manrey@gmail.com>
2020-06-30 02:21:17 -0300

    fix al config.js para que use los valores correctos del archivo env

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ const envs = {
       client: process.env.DEV_DB_CLIENT,
       dbPath: process.env.DEV_DB_PATH
   },
-    purchasesPdfUrl: process.env.DEV_URL,
-    fileStorageUrl: process.env.DEV_PDFURL,
+    purchasesPdfUrl: process.env.DEV_PDFURL,
+    fileStorageUrl: process.env.DEV_URL,
     mode: process.env.DEV_MODE,
     twilio: {
       accountSid: process.env.DEV_TWILIO_ACCOUNTSID,
@@ -20,8 +20,8 @@ const envs = {
       client: process.env.PROD_DB_CLIENT,
       dbPath: process.env.PROD_DB_PATH
   },
-    purchasesPdfUrl: process.env.PROD_URL,
-    fileStorageUrl: process.env.PROD_PDFURL,
+    purchasesPdfUrl: process.env.PROD_PDFURL,
+    fileStorageUrl: process.env.PROD_URL,
     mode: process.env.PROD_MODE,
     twilio: {
       accountSid: process.env.PROD_TWILIO_ACCOUNTSID,
@@ -40,14 +40,3 @@ const config = {
 }
 
 export default config
-
-
-/* export default {
-  purchasesPdfUrl: "src/server/pdf/generated",
-  fileStorageUrl: "http://localhost:5000/",
-  twilio: {
-    accountSid: 'AC4ad2c99f8af10c473575dde6d9dedfd6',
-    authToken: '5f057a945a9e1279d2d2e90672734a1a',
-    sender: '+12038710956'
-  }
-} */
